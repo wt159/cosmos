50bc668e2283f774de28283e69df0010df931443
wt159 <15908282659@163.com>
2023-06-04 21:24:55 +0800

    decltype关键字用法：编译时推导出表达式的类型

--- cpp11book/1/1-3/1-3/Test.cpp
+++ cpp11book/1/1-3/1-3/Test.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 
-class Foo
-{
+class Foo {
 public:
-	static const int Number = 0;
-	int x;
+    static const int Number = 0;
+    int x;
 };
 
 int main(void)
 {
-	int n = 0;
-	volatile const int &x = n;
+    int n = 0;
+    volatile const int& x = n;
 
-	decltype(n) a = n;         //a -> int
-	decltype(x) b = n;         //b -> const volatile int &
+    decltype(n) a = n; // a -> int
+    decltype(x) b = n; // b -> const volatile int &
 
-	decltype(Foo::Number) c = 0;  //c -> const int
+    decltype(Foo::Number) c = 0; // c -> const int
 
-	system("pause");
-	return 0;
+    return 0;
 }
