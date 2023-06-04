41cb1a1089ff644c8d9763aa317adef66fa62a06
wt159 <15908282659@163.com>
2023-06-04 21:29:19 +0800

    列表初始化的用法

--- cpp11book/1/1-7/1-7/Test.cpp
+++ cpp11book/1/1-7/1-7/Test.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 
-int i_arr[3] = { 1, 2, 3 };  //普通数组
+int i_arr[3] = { 1, 2, 3 }; //普通数组
 
-struct A
-{
-	int x;
-	struct B
-	{
-		int i;
-		int j;
-	} b;
-} a = { 1, {2, 3} };  //POD类型
+struct A {
+    int x;
+    struct B {
+        int i;
+        int j;
+    } b;
+} a = { 1, { 2, 3 } }; // POD类型
 
 int i = 0;
 
-class Foo
-{
+class Foo {
 public:
-	Foo(int){}
+    Foo(int) { }
 } foo = 123;
 
 int j(0);
@@ -27,6 +24,5 @@ Foo bar(123);
 int main(void)
 {
 
-	system("pause");
-	return 0;
+    return 0;
 }
