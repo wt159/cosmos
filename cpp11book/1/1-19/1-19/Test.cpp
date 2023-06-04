3e20aea5cbe51c42863b2ee649d9a4e371387f7a
wt159 <15908282659@163.com>
2023-06-04 21:49:05 +0800

    可调用对象 std:function bind lambda

--- cpp11book/1/1-19/1-19/Test.cpp
+++ cpp11book/1/1-19/1-19/Test.cpp
@@ -1,28 +1,27 @@
-#include <iostream>
 #include <functional>
+#include <iostream>
+
 
 void call_when_even(int x, const std::function<void(int)>& f)
 {
-	if (!(x & 1))  //x % 2 == 0
-	{
-		f(x);
-	}
+    if (!(x & 1)) // x % 2 == 0
+    {
+        f(x);
+    }
 }
 
 void output(int x)
 {
-	std::cout << x << " ";
+    std::cout << x << " ";
 }
 
 int main(void)
 {
-	for (int i = 0; i < 10; ++i)
-	{
-		call_when_even(i, output);
-	}
+    for (int i = 0; i < 10; ++i) {
+        call_when_even(i, output);
+    }
 
-	std::cout << std::endl;
+    std::cout << std::endl;
 
-	system("pause");
-	return 0;
+    return 0;
 }
--- cpp11book/1/1-22/1-22/test.cpp
+++ cpp11book/1/1-22/1-22/test.cpp
@@ -1,29 +1,27 @@
-#include <iostream>
 #include <functional>
+#include <iostream>
 
-class A
-{
+
+class A {
 public:
-	int i_ = 0;
+    int i_ = 0;
 
-	void output(int x, int y)
-	{
-		std::cout << x << " " << y << std::endl;
-	}
+    void output(int x, int y)
+    {
+        std::cout << x << " " << y << std::endl;
+    }
 };
 
 int main(void)
 {
-	A a;
-	std::function<void(int, int)> fr =
-		std::bind(&A::output, &a, std::placeholders::_1, std::placeholders::_2);
-	fr(1, 2);  //输出 1 2
+    A a;
+    std::function<void(int, int)> fr = std::bind(&A::output, &a, std::placeholders::_1, std::placeholders::_2);
+    fr(1, 2); //输出 1 2
 
-	std::function<int&(void)> fr_i = std::bind(&A::i_, &a);  //vs13的bug，绑定成员变量要报错
-	fr_i() = 123;
+    std::function<int&(void)> fr_i = std::bind(&A::i_, &a); 
+    fr_i() = 123;
 
-	std::cout << a.i_ << std::endl;  //输出 123
+    std::cout << a.i_ << std::endl; //输出 123
 
-	system("pause");
-	return 0;
+    return 0;
 }
