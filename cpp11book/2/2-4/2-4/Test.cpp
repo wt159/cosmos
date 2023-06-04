8a4e2b3982803ab5546b34ea0745934106d666e4
wt159 <15908282659@163.com>
2023-06-04 22:07:32 +0800

    move语义 forward和完美转发

--- cpp11book/2/2-4/2-4/Test.cpp
+++ cpp11book/2/2-4/2-4/Test.cpp
@@ -2,35 +2,35 @@
 
 void PrintT(int& t)
 {
-	std::cout << "lvalue" << std::endl;
+    std::cout << "lvalue" << std::endl;
 }
 
 template <typename T>
 void PrintT(T&& t)
 {
-	std::cout << "rvalue" << std::endl;
+    std::cout << "rvalue" << std::endl;
 }
 
 template <typename T>
 void TestForward(T&& v)
 {
-	PrintT(v);
-	PrintT(std::forward<T>(v));
-	PrintT(std::move(v));
+    PrintT(v);
+    PrintT(std::forward<T>(v));
+    PrintT(std::move(v));
+    std::cout << "-----------" << std::endl;
 }
 
 void Test()
 {
-	TestForward(1);
-	int x = 1;
-	TestForward(x);
-	TestForward(std::forward<int>(x));
+    TestForward(1);
+    int x = 1;
+    TestForward(x);
+    TestForward(std::forward<int>(x));
 }
 
 int main(void)
 {
-	Test();
+    Test();
 
-	system("pause");
-	return 0;
+    return 0;
 }
